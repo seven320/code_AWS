9d77a5108d73bf5999fd1511bf3ee49cbd3f07ab
seven320 <yosyuaomenw@yahoo.co.jp>
2018-04-29 20:47:16 +0900

    Initial commit

---
+++ othertimeline.py
@@ -0,0 +1,19 @@
+#encoding:utf-8
+import tweepy
+
+#親ディレクトリにあるアカウント情報へのパス
+import sys,os
+
+
+#account情報をaccount.pyからロード
+from account import account #load account
+def AWS_handler(event, context):
+    pardir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(pardir)
+    auth=account.Initialize()
+    api = tweepy.API(auth)
+    twitter_id=account.id()
+
+    othertimelines=api.user_timeline("togetter_jp")
+    for tweet in othertimelines:
+        print(tweet.text)
